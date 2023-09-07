81a8f482e4dc5555c442b23039b65ffb7f49ca72
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2023-09-07 11:27:59 +0800

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('shop.index');
+    }
+
+    public function show()
+    {
+        return view('shop.show');
+    }
+}
